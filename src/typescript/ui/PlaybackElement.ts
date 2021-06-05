807af61ea26d38b1d55623eb57cc248ddf87aa37
PHOENIX\Loggi <loggichtit@gmail.com>
2021-06-05 17:12:10 +0200

    poprawki

--- src/typescript/ui/PlaybackElement.ts
+++ src/typescript/ui/PlaybackElement.ts
@@ -24,18 +24,19 @@ export class PlaybackElement extends GraphicElement {
     protected draw() : void{
         super.draw();
 
-        this.htmlElement.innerHTML = `<?xml version="1.0" encoding="UTF-8"?>
-<svg width="92px" height="92px" viewBox="0 0 72 72" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <title>play_arrow_black_24dp</title>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="PLAYER-MOBILE-@-pause" transform="translate(-144.000000, -284.000000)">
-            <g id="play_arrow_black_24dp" transform="translate(144.000000, 284.000000)">
-                <polygon id="Path" points="0 0 72 0 72 72 0 72"></polygon>
-                <polygon id="Path" fill="#FFFFFF" fill-rule="nonzero" points="24 15 24 57 57 36"></polygon>
-            </g>
-        </g>
-    </g>
-</svg>`;
+
+        this.htmlElement.innerHTML = `<svg class="sp-play-icon" width="43" height="53" viewBox="0 0 72 72">
+                    <g  fill="#fff" fill-rule="evenodd">
+                      <g id="path" fill="#fff">
+                        <g>
+                          <path
+                            d="M13.51 7.681l9.252 13.57c.945 1.386.587 3.276-.799 4.221-.504.344-1.1.528-1.711.528H1.748C.07 26-1.29 24.64-1.29 22.962c0-.61.184-1.207.528-1.711L8.49 7.68c.945-1.386 2.835-1.743 4.221-.798.314.214.585.485.8.798z"
+                            transform="translate(-221 -954) translate(290 947) rotate(90 11 15) scale(3.6)" />
+                        </g>
+                      </g>
+                    </g>    
+                  </svg>
+                  `;
 
         this.hide();
 
