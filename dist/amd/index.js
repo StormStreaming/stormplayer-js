f83148f73a991cfd72bc7aea59d10d1ab5349e35
Szymon Polok <polok.szymon@gmail.com>
2022-01-06 04:59:27 +0100

    Some changes

--- dist/amd/index.js
+++ dist/amd/index.js
@@ -1882,17 +1882,17 @@ define(['exports'], (function (exports) { 'use strict';
    * Class represents the FullScreen button
    */
 
-  var FullScreenElement = /*#__PURE__*/function (_GraphicElement) {
-    _inherits$1(FullScreenElement, _GraphicElement);
+  var FullscreenElement = /*#__PURE__*/function (_GraphicElement) {
+    _inherits$1(FullscreenElement, _GraphicElement);
 
-    var _super = _createSuper$7(FullScreenElement);
+    var _super = _createSuper$7(FullscreenElement);
 
     /**
      * Constructor
      * @param stormPlayer reference to the player main class
      */
-    function FullScreenElement(stormPlayer) {
-      _classCallCheck$1(this, FullScreenElement);
+    function FullscreenElement(stormPlayer) {
+      _classCallCheck$1(this, FullscreenElement);
 
       return _super.call(this, stormPlayer, "sp-controls__fullscreen", "button");
     }
@@ -1902,10 +1902,10 @@ define(['exports'], (function (exports) { 'use strict';
      */
 
 
-    _createClass$1(FullScreenElement, [{
+    _createClass$1(FullscreenElement, [{
       key: "draw",
       value: function draw() {
-        _get$1(_getPrototypeOf$1(FullScreenElement.prototype), "draw", this).call(this);
+        _get$1(_getPrototypeOf$1(FullscreenElement.prototype), "draw", this).call(this);
 
         this.htmlElement.innerHTML = "\n          <svg class=\"fullscreen-icon\" xmlns=\"http://www.w3.org/2000/svg\" width=\"27\" height=\"27\" viewBox=\"0 0 27 27\">\n            <g fill=\"none\" fill-rule=\"evenodd\">\n              <g fill=\"#FFF\" fill-rule=\"nonzero\">\n                <path\n                  d=\"M1.5 17c.828 0 1.5.672 1.5 1.5v5h5c.828 0 1.5.672 1.5 1.5s-.672 1.5-1.5 1.5H1.5C.672 26.5 0 25.828 0 25v-6.5c0-.828.672-1.5 1.5-1.5zM25 17c.828 0 1.5.672 1.5 1.5V25c0 .828-.672 1.5-1.5 1.5h-6.5c-.828 0-1.5-.672-1.5-1.5s.672-1.5 1.5-1.5h5v-5c0-.828.672-1.5 1.5-1.5zM8 0c.828 0 1.5.672 1.5 1.5S8.828 3 8 3H3v5c0 .828-.672 1.5-1.5 1.5S0 8.828 0 8V1.5C0 .672.672 0 1.5 0zm17 0c.828 0 1.5.672 1.5 1.5V8c0 .828-.672 1.5-1.5 1.5s-1.5-.672-1.5-1.5V3h-5c-.828 0-1.5-.672-1.5-1.5S17.672 0 18.5 0H25z\"\n                  transform=\"translate(-1358 -956) translate(1358 956)\" />\n              </g>\n            </g>\n          </svg>\n          \n          <svg class=\"close-fullscreen-icon\" xmlns=\"http://www.w3.org/2000/svg\" width=\"27\" height=\"27\" viewBox=\"0 0 27 27\">\n          <path d=\"M17.321,25.471V18.849a1.528,1.528,0,0,1,1.528-1.528h6.622a1.528,1.528,0,1,1,0,3.056H20.377v5.094a1.528,1.528,0,1,1-3.056,0Zm-10.7,0V20.377H1.529a1.528,1.528,0,1,1,0-3.056H8.151A1.528,1.528,0,0,1,9.68,18.849v6.622a1.528,1.528,0,1,1-3.056,0ZM18.849,9.68a1.528,1.528,0,0,1-1.528-1.528V1.529a1.528,1.528,0,1,1,3.056,0V6.623h5.094a1.528,1.528,0,1,1,0,3.056Zm-17.32,0a1.528,1.528,0,1,1,0-3.056H6.623V1.529a1.528,1.528,0,1,1,3.056,0V8.151A1.528,1.528,0,0,1,8.151,9.68Z\" fill=\"#fff\"/>\n          </svg>";
       }
@@ -1932,7 +1932,7 @@ define(['exports'], (function (exports) { 'use strict';
       }
     }]);
 
-    return FullScreenElement;
+    return FullscreenElement;
   }(GraphicElement);
 
   function _createSuper$6(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct$7(); return function _createSuperInternal() { var Super = _getPrototypeOf$1(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf$1(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn$1(this, result); }; }
@@ -2009,7 +2009,7 @@ define(['exports'], (function (exports) { 'use strict';
         this.leftWrapper.getHtmlElement().appendChild(this.volumeElement.getHtmlElement());
         this.qualityElement = new QualityElement(this.stormPlayer);
         this.leftWrapper.getHtmlElement().appendChild(this.qualityElement.getHtmlElement());
-        this.fullscreenElement = new FullScreenElement(this.stormPlayer);
+        this.fullscreenElement = new FullscreenElement(this.stormPlayer);
         this.rightWrapper.getHtmlElement().appendChild(this.fullscreenElement.getHtmlElement());
       }
     }]);
--- dist/types/ui/controlbuttons/FullscreenElement.d.ts
+++ dist/types/ui/controlbuttons/FullscreenElement.d.ts
@@ -1,6 +1,6 @@
 import { GraphicElement } from "../GraphicElement";
 import { StormPlayer } from "../../StormPlayer";
-export declare class FullScreenElement extends GraphicElement {
+export declare class FullscreenElement extends GraphicElement {
     constructor(stormPlayer: StormPlayer);
     protected draw(): void;
     protected attachListeners(): void;
