9626dcc5d7cc8dd94a05b66b4f95f4be73bd66cc
Szymon Polok <polok.szymon@gmail.com>
2023-07-29 20:51:22 +0200

    Poprawki, poprawek

--- dist/types/events/Dispatcher.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Dispatcher {
-    private events;
-    addEventListener(event: string | number, callback: any): boolean;
-    removeEventListener(event: string | number, callback?: any): boolean;
-    dispatch(event: any, details?: any): boolean;
-}
